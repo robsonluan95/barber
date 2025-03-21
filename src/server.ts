ae662b3c011d13ac483dcccf24d9fdfe4d6ca40c
Luan <rluan95@hotmail.com>
2025-03-20 21:37:36 -0300

    att

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,7 @@ import cors from "cors"
 import {router} from './routes'
 const app = express()
 app.use(express.json())
-
+app.use(cors())
 app.use(router)
 
 app.use((err:Error,req:Request,res:Response,next:NextFunction)=>{
